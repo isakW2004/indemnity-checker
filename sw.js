8e7a3c57bc583f8ec3854fae99324ab166039a04
Isak Winter <isakmwinter@gmail.com>
2023-12-22 11:04:37 -0600

    Service Worker Changes
    The service worker will now try to get the most recent page updates, then draw from cache if the browser is offline.

--- sw.js
+++ sw.js
@@ -10,7 +10,8 @@ var urlsToCache = [
   "logos/fischer.svg",
   "logos/head.svg",
   "logos/look.svg",
-  "logos/marker.svg"
+  "logos/marker.svg",
+  "logos/salomon-atomic.svg"
 ];
 
 self.addEventListener('install', function(event) {
@@ -25,36 +26,20 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-
-        return fetch(event.request).then(
-          function(response) {
-            // Check if we received a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-            var responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
-      })
-    );
+    fetch(event.request).then(function(response){
+      if(!response || response.status !== 200 || response.type !== 'basic') {
+        return response;
+      }
+      var responseToCache = response.clone();
+      caches.open(CACHE_NAME)
+        .then(function(cache) {
+          cache.put(event.request, responseToCache);
+        });
+      return response;
+    }).catch(function() {
+      return caches.match(event.request);
+    })
+  );
 });
 self.addEventListener('activate', function(event) {
 
